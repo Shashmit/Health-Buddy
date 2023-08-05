223089391f32358a4a3a3b9d1f968e5588f54d16
Shashmit kr <shaashmit@gmail.com>
2023-08-05 11:16:16 +0530

    Build Schema and connect the post request to the Mongoose

--- Backend/routes/workouts.js
+++ Backend/routes/workouts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Workout = require("../models/workoutModels");
 
 router.get("/", (req, res) => {
   res.json({ msg: "get all the workout" });
@@ -11,8 +12,18 @@ router.get("/:id", (req, res) => {
   });
 });
 //Post a new workout
-router.post("/", (req, res) => {
-  res.json({ msg: "posting a new workout" });
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+  try {
+    const workout = await Workout.create({
+      title,
+      load,
+      reps,
+    });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 //Delete a workout
 router.delete("/:id", (req, res) => {
